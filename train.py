8d964541e0c753730197f730ba3f21d775673ce6
EliotParkRL <eliot.park@roxburylatin.org>
2024-09-23 10:29:36 -0400

    rename and repalce babip

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from typing import List
 
 import pandas as pd
 
-X_train = pd.read_csv("All batted ball data 2024.csv",usecols=["launch_speed", 'launch_angle'])
-y_train = pd.read_csv("All batted ball data 2024.csv",usecols=["babip_value"])
+X_train = pd.read_csv("data.csv",usecols=["launch_speed", 'launch_angle'])
+y_train = pd.read_csv("data.csv",usecols=["babip_value"])
 print(X_train)
 print(y_train)
