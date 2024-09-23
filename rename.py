0bdadc43a8a54aad70e1481b0aac5c709d71a293
EliotParkRL <eliot.park@roxburylatin.org>
2024-09-23 10:31:08 -0400

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,13 +1,10 @@
 import pandas as pd
 
-# Read the CSV file
+
 data = pd.read_csv("data.csv")
 
-# Rename the column 'babip_value' to 'hit'
 data.rename(columns={'babip_value': 'hit'}, inplace=True)
 
-# Save the modified dataframe back to the CSV file
 data.to_csv("data.csv", index=False)
 
-# Optionally, print the modified data to verify
 print(data)
