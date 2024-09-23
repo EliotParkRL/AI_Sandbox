5f73df49cef824a7de6f8b1fd2daf65b08a26936
NomzNomz <calvin.reid@roxburylatin.org>
2024-09-23 10:37:09 -0400

    changed to numpy arrays

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
 from typing import List
 
 import pandas as pd
+import numpy as np
 
-X_train = pd.read_csv("data.csv",usecols=["launch_speed", 'launch_angle'])
-y_train = pd.read_csv("data.csv",usecols=["hit"])
+X_train = np.array(pd.read_csv("data.csv",usecols=["launch_speed", 'launch_angle']))
+y_train = np.array(pd.read_csv("data.csv",usecols=["hit"]))
+
 print(X_train)
 print(y_train)
