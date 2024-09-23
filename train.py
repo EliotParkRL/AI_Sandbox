b682c17d6e3ac55386b0b0f7f7a18afc622cf802
Nathan Zhang <nathan.zhang@roxburylatin.org>
2024-09-23 10:54:15 -0400

    random

--- train.py
+++ train.py
@@ -36,3 +36,34 @@ def compute_cost(X, y, w, b):
     total_cost = total_cost / m
 
     return total_cost
+
+
+def compute_gradient_logistic(X, y, w, b):
+    """
+    Computes the gradient for logistic regression
+
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters
+      b (scalar)      : model parameter
+    Returns
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w.
+      dj_db (scalar)      : The gradient of the cost w.r.t. the parameter b.
+    """
+    m, n = X.shape
+    dj_dw = np.zeros((n,))  # (n,)
+    dj_db = 0.
+
+    for i in range(m):
+        f_wb_i = sigmoid(np.dot(X[i], w) + b)  # (n,)(n,)=scalar
+        err_i = f_wb_i - y[i]  # scalar
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err_i * X[i, j]  # scalar
+        dj_db = dj_db + err_i
+    dj_dw = dj_dw / m  # (n,)
+    dj_db = dj_db / m  # scalar
+
+    return dj_db, dj_dw
+
+print(compute_gradient_logistic(X_train, y_train, [1, 1], 2))
