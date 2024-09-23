7602e28099d4091bd35c4cb26770ea9ab4112667
NomzNomz <calvin.reid@roxburylatin.org>
2024-09-23 10:31:54 -0400

    fixed headers

--- train.py
+++ train.py
@@ -3,6 +3,6 @@ from typing import List
 import pandas as pd
 
 X_train = pd.read_csv("data.csv",usecols=["launch_speed", 'launch_angle'])
-y_train = pd.read_csv("data.csv",usecols=["babip_value"])
+y_train = pd.read_csv("data.csv",usecols=["hit"])
 print(X_train)
 print(y_train)
