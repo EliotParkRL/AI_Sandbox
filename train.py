9eb53b9f274a029ab88da642bc17037599fd6f32
NomzNomz <calvin.reid@roxburylatin.org>
2024-09-23 10:23:52 -0400

    made x_train and y_train

--- train.py
+++ train.py
@@ -1,4 +1,8 @@
+from typing import List
+
 import pandas as pd
 
-x_train = pd.read_csv("All batted ball data 2024.csv")
-print(x_train)
+X_train = pd.read_csv("All batted ball data 2024.csv",usecols=["launch_speed", 'launch_angle'])
+y_train = pd.read_csv("All batted ball data 2024.csv",usecols=["babip_value"])
+print(X_train)
+print(y_train)
